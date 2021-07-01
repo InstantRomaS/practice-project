e8491c7068314914ab33d40ce8dc197a0406e3a6
InstantRomaS <tronkreeper@mail.ru>
2021-07-01 16:42:35 +0300

    feat commit

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Mogilev&lang=ru&units=metric&appid=1cfada82e70e7faaaa7e751061a54e46')
+    .then(function(resp){ return resp.json() })
+    .then(function(data) {
+        console.log(data);
+        document.querySelector('.city-name').innerHTML=data.name;
+        document.querySelector('.temperature').innerHTML=Math.round(data.main.temp)+'&deg';
+        document.querySelector('.wind').innerHTML=data.weather[0].description;
+        document.querySelector('.icon').innerHTML=` <img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"`;
+    })
+    .catch(function(){
+    });
