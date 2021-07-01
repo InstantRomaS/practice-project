5e8291d00c155fe22d1b2b096b55b239da98f947
InstantRomaS <tronkreeper@mail.ru>
2021-07-02 01:44:06 +0300

    feature commit

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Mogilev&lang=ru&units=me
         document.querySelector('.city-name').innerHTML=data.name;
         document.querySelector('.temperature').innerHTML=Math.round(data.main.temp)+'&deg';
         document.querySelector('.wind').innerHTML=data.weather[0].description;
-        document.querySelector('.icon').innerHTML=` <img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"`;
+        document.querySelector('.icon').innerHTML=`<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
     })
     .catch(function(){
     });
+/*async function getWeatherByLocation(location){
+    const resp = await fetch(url(location),{
+        origin: "cors"
+    });
+    const respData = await resp.json();
+}*/
