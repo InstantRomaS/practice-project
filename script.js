8482eab92a49b8772dc1676fa47fa61e696342fa
InstantRomaS <tronkreeper@mail.ru>
2021-07-02 14:20:18 +0300

    feat: add search form

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Mogilev&lang=ru&units=metric&appid=1cfada82e70e7faaaa7e751061a54e46')
+const defaultCity = 'Mogilev';
+const weatherApiUtl = 'http://api.openweathermap.org/data/2.5/weather';
+
+const cityNameElement = document.querySelector('.city-name');
+
+const insertIcon = (iconCipher) => `<img src="https://openweathermap.org/img/wn/${iconCipher}@2x.png">`;
+
+fetch(`${weatherApiUtl}?q=${defaultCity}&lang=ru&units=metric&appid=1cfada82e70e7faaaa7e751061a54e46`)
     .then(function(resp){ return resp.json() })
     .then(function(data) {
         console.log(data);
-        document.querySelector('.city-name').innerHTML=data.name;
-        document.querySelector('.temperature').innerHTML=Math.round(data.main.temp)+'&deg';
+        cityNameElement.innerHTML = data.name;
+        document.querySelector('.temperature').innerHTML=Math.round(data.main.temp) + '&degC';
         document.querySelector('.wind').innerHTML=data.weather[0].description;
-        document.querySelector('.icon').innerHTML=`<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+        document.querySelector('.icon').innerHTML = insertIcon(data.weather[0]['icon']);
     })
     .catch(function(){
     });
-/*async function getWeatherByLocation(location){
-    const resp = await fetch(url(location),{
-        origin: "cors"
-    });
+/*async function getWeatherByLocation(){
+    let city=document.getElementById("sear").nodeValue;
+    let URL=`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=1cfada82e70e7faaaa7e751061a54e46`;
+    const resp = await fetch(URL,
     const respData = await resp.json();
 }*/
