f3c114d8127e9656f15f9e753d8897da6012c563
InstantRomaS <tronkreeper@mail.ru>
2021-07-07 01:47:51 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// import {DAY_ARRAY} from './consts';
-
-// console.log(DAY_ARRAY);
-
-const form1 = document.getElementById("form1");
-const search = document.getElementById("search");
-const defaultCity = 'Mogilev';
-const currDate = new Date();
-console.log(currDate);
-
-const weatherAPIKEY = '&appid=1cfada82e70e7faaaa7e751061a54e46';
-const weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const weathersearchAPIUrl='http://api.openweathermap.org/data/2.5/forecast';
-const insertIcon = (iconCipher) => `<img src="https://openweathermap.org/img/wn/${iconCipher}@2x.png">`;
-
-const forecast = document.querySelector('.showforecast');
-const cityNameElement = document.querySelector('.city-name');
-const temperatureEl = document.querySelector('.temperature');
-const windEl = document.querySelector('.wind');
-const iconEl = document.querySelector('.icon');
-
+ import {DAY_ARRAY, weatherAPIKEY, weatherApiUrl, insertIcon, weathersearchAPIUrl, form1, search, defaultCity, forecast, cityNameElement, temperatureEl, windEl, iconEl} from './consts.js';
 
 const createForecastSection = (respData) => {
     const il = document.createElement("ul");
@@ -28,16 +8,12 @@ const createForecastSection = (respData) => {
         const {main:{temp}, weather:{0: {description, icon}}, dt_txt} = element;
         const dateAPI = new Date(dt_txt);
         if(dateAPI.getHours() == 15){
-            const dayArray = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-
             const forecastEl = document.createElement
             ("li");
             forecastEl.classList.add("weatherDaily");
             forecastEl.innerHTML = `
-                    <span>${dayArray[dateAPI.getDay()]}</span>
-                    <img
-                        src="https://openweathermap.org/img/wn/${icon}@2x.png"
-                    >
+                    <span>${DAY_ARRAY[dateAPI.getDay()]}</span>
+                    ${insertIcon(icon)}
                     <div class="weather-info">
                         <h3>${Math.round(temp)}</h3>
                         <span>${description}</span>
@@ -65,14 +41,15 @@ fetch(`${weatherApiUrl}?q=${defaultCity}&lang=ru&units=metric${weatherAPIKEY}`)
 form1.addEventListener("submit", (e) => {
     forecast.innerHTML = "";
     e.preventDefault();
-    const searchTerm = search.value;
+    let searchTerm = search.value;
     fetch(`${weathersearchAPIUrl}?q=${searchTerm}&lang=ru&units=metric&day=5${weatherAPIKEY}`)
         .then((resp) => resp.json())
         .then((respData) => {
             console.log(respData);
             createForecastSection(respData);
         })
-        .catch(function(){
+        .catch(function(err){
+            console.log(err);
         });
     if (searchTerm) {
         search.value = "";
