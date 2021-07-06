8ca089c78bbf91240f743eaf9780bbf142e62345
InstantRomaS <tronkreeper@mail.ru>
2021-07-06 16:36:06 +0300

    redactoring

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// import {DAY_ARRAY} from './consts';
+
+// console.log(DAY_ARRAY);
+
 const form1 = document.getElementById("form1");
 const search = document.getElementById("search");
 const defaultCity = 'Mogilev';
@@ -16,52 +20,57 @@ const windEl = document.querySelector('.wind');
 const iconEl = document.querySelector('.icon');
 
 
+const createForecastSection = (respData) => {
+    const il = document.createElement("ul");
+    il.classList.add('il');
+    document.querySelector('.showforecast').appendChild(il);
+    respData.list.forEach((element) => {
+        const {main:{temp}, weather:{0: {description, icon}}, dt_txt} = element;
+        const dateAPI = new Date(dt_txt);
+        if(dateAPI.getHours() == 15){
+            const dayArray = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+            const forecastEl = document.createElement
+            ("li");
+            forecastEl.classList.add("weatherDaily");
+            forecastEl.innerHTML = `
+                    <span>${dayArray[dateAPI.getDay()]}</span>
+                    <img
+                        src="https://openweathermap.org/img/wn/${icon}@2x.png"
+                    >
+                    <div class="weather-info">
+                        <h3>${Math.round(temp)}</h3>
+                        <span>${description}</span>
+                    </div>
+            `;
+
+            document.querySelector('.il').appendChild(forecastEl);
+        }
+    });
+};
+
 fetch(`${weatherApiUrl}?q=${defaultCity}&lang=ru&units=metric${weatherAPIKEY}`)
-    .then(function(resp){ return resp.json() })
-    .then(function(data) {
+    .then((resp) => resp.json())
+    .then((data) => {
         console.log(data);
         cityNameElement.innerHTML = data.name;
         temperatureEl.innerHTML = Math.round(data.main.temp) + '&degC';
         windEl.innerHTML = data.weather[0].description;
         iconEl.innerHTML = insertIcon(data.weather[0]['icon']);
     })
-    .catch(function(){
+    .catch((err) => {
+        console.log(err);
     });
+
 form1.addEventListener("submit", (e) => {
     forecast.innerHTML = "";
     e.preventDefault();
     const searchTerm = search.value;
     fetch(`${weathersearchAPIUrl}?q=${searchTerm}&lang=ru&units=metric&day=5${weatherAPIKEY}`)
-        .then(function(resp) { return resp.json() })
-        .then(function(respData) {
+        .then((resp) => resp.json())
+        .then((respData) => {
             console.log(respData);
-            const il = document.createElement
-            ("ul");
-            il.classList.add('il');
-            document.querySelector('.showforecast').appendChild(il);
-            respData.list.forEach((element) => {
-                const {main:{temp}, weather:{0: {description, icon}}, dt_txt} = element;
-                const dateAPI = new Date(dt_txt);
-                if(dateAPI.getHours() == 15){
-                    const dayArray = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-
-                    const forecastEl = document.createElement
-                    ("li");
-                    forecastEl.classList.add("weatherDaily");
-                    forecastEl.innerHTML = `
-                            <span>${dayArray[dateAPI.getDay()]}</span>
-                            <img
-                                src="https://openweathermap.org/img/wn/${icon}@2x.png"
-                            >
-                            <div class="weather-info">
-                                <h3>${Math.round(temp)}</h3>
-                                <span>${description}</span>
-                            </div>
-                    `;
-    
-                    document.querySelector('.il').appendChild(forecastEl);
-                }
-            });
+            createForecastSection(respData);
         })
         .catch(function(){
         });
