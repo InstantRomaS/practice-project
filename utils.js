bfe430e0590dd5958495185742f2cb9b36547c27
InstantRomaS <tronkreeper@mail.ru>
2021-07-08 20:41:29 +0300

    feature: slider

--- utils.js
+++ utils.js
@@ -1,9 +1,12 @@
 import { DAY_ARRAY, insertIcon, } from './consts.js';
 
 export const createForecastSection = (respData) => {
+    const slider_container = document.createElement("div");
+    slider_container.classList.add('slider-container');
+    document.querySelector('.showforecast').appendChild(slider_container);
     const il = document.createElement("ul");
     il.classList.add('il');
-    document.querySelector('.showforecast').appendChild(il);
+    document.querySelector('.slider-container').appendChild(il);
     respData.list.forEach((element) => {
         const {main:{temp}, weather:{0: {description, icon}}, dt_txt} = element;
         const dateAPI = new Date(dt_txt);
@@ -23,4 +26,15 @@ export const createForecastSection = (respData) => {
             document.querySelector('.il').appendChild(forecastEl);
         }
     });
+    scrollBtns();
 };
+
+function scrollBtns() {
+    const slider_buttons = document.createElement("div");
+    slider_buttons.classList.add("slider_buttons");
+    slider_buttons.innerHTML = `
+            <button class="btn_prev">Пред</button>
+            <button class="btn_next">След</button>
+    `;
+    document.querySelector('.showforecast').appendChild(slider_buttons);
+}
